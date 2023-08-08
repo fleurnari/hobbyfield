6375628f920684c61434494c01ee29e860d9bc63
fleurnari <fleur_nr@naver.com>
2023-08-08 18:15:14 +0900

    230808 - 회원가입 구현 중

---
+++ hobbyfield/src/main/java/com/hobbyfield/app/member/service/MemberService.java
@@ -0,0 +1,11 @@
+package com.hobbyfield.app.member.service;
+
+public interface MemberService {
+	
+	// 회원 가입 수행
+	public int insertMember(MemberVO memberVO);
+	
+	// 아이디 중복 체크
+	public int idChk(MemberVO memberVO);
+	
+}
